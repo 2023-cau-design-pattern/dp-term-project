9ee73895e658601d9468bb8400515469bd0ea1e1
Ukj0ng <xb253q@gmail.com>
2023-11-21 17:38:57 +0900

    feat(InputView): 사용자의 입력을 받아 반환
    구현한 기능:
    - 입력받은 빵 반환
    - 입력받은 패티 반환
    - 입력받은 토핑 반

--- src/com/holub/application/presentation/InputView.java
+++ src/com/holub/application/presentation/InputView.java
@@ -1,2 +1,32 @@
-package com.holub.application.presentation;public class InputVIew {
+package com.holub.application.presentation;
+
+import com.holub.application.presentation.message.ConsoleMessage;
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+    public void greeting() {
+        System.out.println(ConsoleMessage.GREETING.getMessage());
+    }
+
+    public String readBread() {
+        System.out.println(ConsoleMessage.BREAD_SELECTION.getMessage());
+        String bread = Console.readLine();
+
+        return bread;
+    }
+
+    public String readPatty() {
+        System.out.println(ConsoleMessage.PATTY_SELECTION.getMessage());
+        String patty = Console.readLine();
+
+        return patty;
+    }
+
+    public List<String> readToppings() {
+        System.out.println(ConsoleMessage.TOPPINGS_SELECTION.getMessage());
+        String toppings = Console.readLine();
+
+        return Arrays.stream(toppings.split(",")).toList();
+    }
 }

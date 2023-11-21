9d1b99084e9b986ab11a7e56e9c4bafdb986fa28
gunom <gu3062@naver.com>
2023-11-21 16:09:47 +0900

    feat: PriceManager 추가

---
+++ src/com/holub/application/service/PriceManager.java
@@ -0,0 +1,41 @@
+package com.holub.application.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PriceManager {
+    private static PriceManager instance;
+    private final Map<String, Double> priceMap;
+
+    private PriceManager() {
+        priceMap = new HashMap<>();
+        loadPrices();
+    }
+
+    public static synchronized PriceManager getInstance() {
+        if (instance == null) {
+            // TODO DB Select 넣고 개선 필요
+            instance = new PriceManager();
+        }
+        return instance;
+    }
+
+    private void loadPrices() {
+        priceMap.put("wheat", 5.50);
+        priceMap.put("white", 5.00);
+        priceMap.put("honey_oat", 6.00);
+        priceMap.put("oat", 5.75);
+        priceMap.put("flatbread", 6.50);
+        priceMap.put("mustard", 0.30);
+        priceMap.put("chili", 0.45);
+        priceMap.put("ranch", 0.50);
+        priceMap.put("tomato", 0.25);
+        priceMap.put("cheese", 0.75);
+        priceMap.put("ham", 1.00);
+
+    }
+
+    public double getPrice(String itemName) {
+        return priceMap.getOrDefault(itemName, 0.0);
+    }
+}

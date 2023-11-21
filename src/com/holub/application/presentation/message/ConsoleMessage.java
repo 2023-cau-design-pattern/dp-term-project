b70e415575d9c7a88f20f4501465fcaed933032e
Ukj0ng <xb253q@gmail.com>
2023-11-21 17:40:43 +0900

    feat(ConsoleMessage): 사용자에게 보여질 콘솔 메세지
    구현한 메세지:
    - xx 햄버거입니다.
    - 빵의 종류를 선택해주세요.
    - 패티의 종류를 선택해주세요.
    - 토핑의 종류를 선택해주세요.
    - 선택한 메뉴에 수정하고 싶은 카테고리가 있나요? (빵/패티/토핑/없음)

--- src/com/holub/application/presentation/message/ConsoleMessage.java
+++ src/com/holub/application/presentation/message/ConsoleMessage.java
@@ -1,2 +1,19 @@
-package com.holub.application.presentation.message;public enum ConsoleMessage {
+package com.holub.application.presentation.message;
+
+public enum ConsoleMessage {
+    GREETING("xx 햄버거입니다."),
+    BREAD_SELECTION("빵의 종류를 선택해주세요."),
+    PATTY_SELECTION("패티의 종류를 선택해주세요."),
+    TOPPINGS_SELECTION("토핑의 종류를 선택해주세요."),
+    ASK_FOR_CHANGE("선택한 메뉴에 수정하고 싶은 카테고리가 있나요? (빵/패티/토핑/없음)");
+
+    private final String message;
+
+    ConsoleMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }

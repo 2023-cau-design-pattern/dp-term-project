60fffbccb9f9ed0084f5b35ca20a761974b7d890
Ukj0ng <xb253q@gmail.com>
2023-11-22 09:32:23 +0900

    feat(ErrorMessage): 에러 메세지
    구현한 메세지:
    - 타당하지 않은 선택입니다. 다시 골라 주세요.

--- src/com/holub/application/presentation/message/ErrorMessage.java
+++ src/com/holub/application/presentation/message/ErrorMessage.java
@@ -1,2 +1,15 @@
-package com.holub.application.presentation.message;public enum ErrorMessage {
+package com.holub.application.presentation.message;
+
+public enum ErrorMessage {
+    INVALID_CHOICE("타당하지 않은 선택입니다. 다시 골라 주세요.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }

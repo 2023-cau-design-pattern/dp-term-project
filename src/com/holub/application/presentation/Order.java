584ca664a17f369f7b0e86644574e314acc1127d
Ukj0ng <xb253q@gmail.com>
2023-11-22 09:31:04 +0900

    feat(Order): service 계층과의 통신을 위한 데이터 객체
    멤버 변수:
    - 빵
    - 패티
    - 토핑

--- src/com/holub/application/presentation/Order.java
+++ src/com/holub/application/presentation/Order.java
@@ -1,2 +1,27 @@
-package com.holub.application.presentation;public class Order {
+package com.holub.application.presentation;
+
+import java.util.List;
+
+public class Order {
+    private final String bread;
+    private final String patty;
+    private final List<String> toppings;
+
+    public Order(String bread, String patty, List<String> toppings) {
+        this.bread = bread;
+        this.patty = patty;
+        this.toppings = toppings;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public String getPatty() {
+        return patty;
+    }
+
+    public List<String> getToppings() {
+        return toppings;
+    }
 }

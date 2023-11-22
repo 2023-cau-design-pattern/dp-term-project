fe018acb8f249eac56bb07ea03ed6587aad637e0
Ukj0ng <xb253q@gmail.com>
2023-11-22 09:28:57 +0900

    feat(ConsoleController): 사용자의 입력 기능을 수행
    구현한 기능:
    - 빵 선택
    - 패티 선택
    - 토핑 선택
    - 현재 고른 재료들 출력
    - 바꿀 재료들 선택

--- src/com/holub/application/presentation/ConsoleController.java
+++ src/com/holub/application/presentation/ConsoleController.java
@@ -1,2 +1,75 @@
-package com.holub.application.presentation;public class ConsoleController {
+package com.holub.application.presentation;
+
+import com.holub.application.presentation.message.ConsoleMessage;
+import com.holub.application.presentation.message.ErrorMessage;
+import java.util.List;
+
+public class ConsoleController {
+    private static final String OPTION_BREAD = "빵";
+    private static final String OPTION_PATTY = "패티";
+    private static final String OPTION_TOPPINGS = "토핑";
+    private static final String OPTION_NONE = "없음";
+    private final InputView inputView = new InputView();
+    private Order order;
+
+    public Order readOrder() {
+        inputView.greeting();
+
+        String bread = null;
+        String patty = null;
+        List<String> toppings = null;
+
+        while (true) {
+            bread = chooseBread(bread);
+            patty = choosePatty(patty);
+            toppings = chooseToppings(toppings);
+
+            String modificationChoice = askForModification();
+            if (modificationChoice.equals(OPTION_NONE)) {
+                return new Order(bread, patty, toppings);
+            }
+            showCurrentChoice(bread, patty, toppings);
+
+            if (modificationChoice.equals(OPTION_BREAD)) {
+                bread = null;
+            }
+            else if (modificationChoice.equals(OPTION_PATTY)) {
+                patty = null;
+            }
+            else if (modificationChoice.equals(OPTION_TOPPINGS)) {
+                toppings = null;
+            }
+        }
+    }
+    private String chooseBread(String bread) {
+        if (bread == null) {
+            return inputView.readBread();
+        }
+        return bread;
+    }
+
+    private String choosePatty(String patty) {
+        if (patty == null) {
+            return inputView.readPatty();
+        }
+        return patty;
+    }
+
+    private List<String> chooseToppings(List<String> toppings) {
+        if (toppings == null) {
+            return inputView.readToppings();
+        }
+        return toppings;
+    }
+
+    private void showCurrentChoice(String bread, String patty, List<String> toppings) {
+        System.out.println("현재 선택하신 메뉴: 빵 - " + bread + ", 패티 - " + patty + ", 토핑 - " + String.join(", ", toppings));
+    }
+
+    private String askForModification() {
+        System.out.println(ConsoleMessage.ASK_FOR_CHANGE.getMessage());
+        String choice = Console.readLine();
+
+        return choice;
+    }
 }

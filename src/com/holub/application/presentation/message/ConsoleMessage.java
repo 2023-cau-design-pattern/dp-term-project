738e1a393e2ed93e64ecbd706f2db4add612f372
Ukj0ng <xb253q@gmail.com>
2023-11-23 17:51:36 +0900

    fix(ConsoleMessage): 변경 문구 수정
    수정 사항:
    - (빵/패티/소스/토핑/음료없음) -> (빵/패티/소스/토핑/음료/없음)

--- src/com/holub/application/presentation/message/ConsoleMessage.java
+++ src/com/holub/application/presentation/message/ConsoleMessage.java
@@ -8,7 +8,7 @@ public enum ConsoleMessage {
     TOPPINGS_SELECTION("토핑의 종류를 선택해주세요."),
     BEVERAGE_SELECTION("음료의 종류를 선택해주세요."),
     ASK_FOR_CHANGE("선택한 메뉴에 수정하고 싶은 카테고리가 있나요? (빵/패티/소스/토핑/음료/없음)");
-    
+
     private final String message;
 
     ConsoleMessage(String message) {

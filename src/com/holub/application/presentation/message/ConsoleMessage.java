0d3ec690adfa544e090dd58868aa4be3e54ddb91
Ukj0ng <xb253q@gmail.com>
2023-11-23 17:23:38 +0900

    feat(ConsoleMessage): 소스, 음료 선택 문구 추가 dquote> dquote> 추가한 문구: dquote> - 소스의 종류를 선택해주세요. dquote> - 음료의 종류를 선택해주세요. dquote> dquote> 변경한 문구: dquote> - 선택한 메뉴에 수정하고 싶은 카테고리가 있나요? (빵/패티/토핑/없음) -> 선택한 메뉴에 수정하고 싶은 카테고리가 있나요? (빵/패티/소스/토핑/음료/없음)

--- src/com/holub/application/presentation/message/ConsoleMessage.java
+++ src/com/holub/application/presentation/message/ConsoleMessage.java
@@ -4,8 +4,10 @@ public enum ConsoleMessage {
     GREETING("xx 햄버거입니다."),
     BREAD_SELECTION("빵의 종류를 선택해주세요."),
     PATTY_SELECTION("패티의 종류를 선택해주세요."),
+    SAUCE_SELECTION("소스의 종류를 선택해주세요."),
     TOPPINGS_SELECTION("토핑의 종류를 선택해주세요."),
-    ASK_FOR_CHANGE("선택한 메뉴에 수정하고 싶은 카테고리가 있나요? (빵/패티/토핑/없음)");
+    BEVERAGE_SELECTION("음료의 종류를 선택해주세요."),
+    ASK_FOR_CHANGE("선택한 메뉴에 수정하고 싶은 카테고리가 있나요? (빵/패티/소스/토핑/음료없음)");
 
     private final String message;
 

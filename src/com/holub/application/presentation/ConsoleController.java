26137a423541d7986de8fc531124f0d7af824177
Ukj0ng <xb253q@gmail.com>
2023-11-23 17:25:44 +0900

    feat(ConsoleController): 소스, 음료 선택 기능 구현
    구현한 기능:
    - 소스 선택 기능
    - 음료 선택 기능

--- src/com/holub/application/presentation/ConsoleController.java
+++ src/com/holub/application/presentation/ConsoleController.java
@@ -7,7 +7,9 @@ import java.util.List;
 public class ConsoleController {
     private static final String OPTION_BREAD = "빵";
     private static final String OPTION_PATTY = "패티";
+    private static final String OPTION_SAUCE = "소스";
     private static final String OPTION_TOPPINGS = "토핑";
+    private static final String OPTION_BEVERAGE = "음료";
     private static final String OPTION_NONE = "없음";
     private final InputView inputView = new InputView();
     private Order order;
@@ -17,16 +19,20 @@ public class ConsoleController {
 
         String bread = null;
         String patty = null;
+        String sauce = null;
         List<String> toppings = null;
+        String beverage = null;
 
         while (true) {
             bread = chooseBread(bread);
             patty = choosePatty(patty);
+            sauce = chooseSauce(sauce);
             toppings = chooseToppings(toppings);
+            beverage = chooseBeverage(beverage);
 
             String modificationChoice = askForModification();
             if (modificationChoice.equals(OPTION_NONE)) {
-                return new Order(bread, patty, toppings);
+                return new Order(bread, patty, sauce, toppings, beverage);
             }
             showCurrentChoice(bread, patty, toppings);
 
@@ -36,8 +42,14 @@ public class ConsoleController {
             else if (modificationChoice.equals(OPTION_PATTY)) {
                 patty = null;
             }
+            else if (modificationChoice.equals(OPTION_SAUCE)) {
+                sauce = null;
+            }
             else if (modificationChoice.equals(OPTION_TOPPINGS)) {
                 toppings = null;
+            }
+            else if (modificationChoice.equals(OPTION_BEVERAGE)) {
+                beverage = null;
             }
         }
     }
@@ -55,11 +67,25 @@ public class ConsoleController {
         return patty;
     }
 
+    private String chooseSauce(String sauce) {
+        if (sauce == null) {
+            return inputView.readSauce();
+        }
+        return sauce;
+    }
+
     private List<String> chooseToppings(List<String> toppings) {
         if (toppings == null) {
             return inputView.readToppings();
         }
         return toppings;
+    }
+
+    private String chooseBeverage(String beverage) {
+        if (beverage == null) {
+            return inputView.readBeverage();
+        }
+        return beverage;
     }
 
     private void showCurrentChoice(String bread, String patty, List<String> toppings) {

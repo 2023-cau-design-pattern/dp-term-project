79e5bad5b365ac76080bd3b9853dad5aacbeca04
Ukj0ng <xb253q@gmail.com>
2023-11-26 17:51:31 +0900

    feat(InputView): 사용자의 결제 입력 기능 구현
    구현한 기능:
    - 사용자의 결제 방법 읽기
    - 결제 진행

--- src/com/holub/application/presentation/InputView.java
+++ src/com/holub/application/presentation/InputView.java
@@ -1,7 +1,10 @@
 package com.holub.application.presentation;
 
 import com.holub.application.presentation.message.ConsoleMessage;
+import com.holub.application.presentation.payment.Payment;
+import com.holub.application.presentation.payment.PaymentMethod;
 import com.holub.application.presentation.validator.InputValidator;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -72,6 +75,42 @@ public class InputView {
             try {
                 inputValidator.validateBeverageType(beverage);
                 return beverage;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public Payment readPayment(double totalPrice) {
+        while (true) {
+            System.out.println(ConsoleMessage.PAYMENT_TYPE.getMessage());
+            System.out.println(ConsoleMessage.PAYMENT_SELECTION.getMessage());
+            String methodInput = Console.readLine();
+
+            try {
+                inputValidator.validatePaymentMethod(methodInput);
+
+                PaymentMethod method = Arrays.stream(PaymentMethod.values())
+                    .filter(m -> m.getMethod().equals(methodInput))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("Invalid payment method"));
+
+                if (PaymentMethod.CASH.equals(method)) {
+                    System.out.println(totalPrice + ConsoleMessage.CASH_PAYMENT.getMessage());
+                }
+                else {
+                    System.out.println(ConsoleMessage.CREDIT_CARD_PAYMENT.getMessage());
+                    System.out.println(ConsoleMessage.CREDIT_CARD_PROCESSING.getMessage());
+
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        System.out.println("An interruption occurred during payment processing.");
+                    }
+                }
+                System.out.println(ConsoleMessage.RESULT_MESSAGE.getMessage());
+                return new Payment(method, totalPrice, LocalDate.now());
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }

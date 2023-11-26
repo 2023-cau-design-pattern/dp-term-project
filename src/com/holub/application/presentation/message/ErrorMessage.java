950a99c5e555dd7559779c73a801cc875cbc9b12
Ukj0ng <xb253q@gmail.com>
2023-11-26 17:53:39 +0900

    feat(ErrorMessage): 에러 메세지 추가
    추가한 메세지:
    - 유효하지 않은 결제 방식입니다. 다시 선택해주세요.

--- src/com/holub/application/presentation/message/ErrorMessage.java
+++ src/com/holub/application/presentation/message/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     INVALID_SAUCE_TYPE("존재하지 않는 소스의 종류입니다. 다시 골라 주세요."),
     INVALID_TOPPINGS_TYPE("존재하지 않는 토핑의 종류입니다. 다시 골라 주세요."),
     INVALID_BEVERAGE_TYPE("존재하지 않는 음료의 종류입니다. 다시 골라 주세요."),
-    INVALID_SELECTION("타당하지 않은 선택입니다. 다시 골라 주세요.");
+    INVALID_SELECTION("타당하지 않은 선택입니다. 다시 골라 주세요."),
+    INVALID_PAYMENT_SELECTION("유효하지 않은 결제 방식입니다. 다시 선택해주세요.");
 
     private final String message;
 

be0edb6751263a014e97bf16957a1850dbaa3f2f
Ukj0ng <xb253q@gmail.com>
2023-11-26 17:47:27 +0900

    feat(PayMethod): 결재 방법 카테고리 추가
    추가한 카테고리:
    - 현금
    - 신용카드

--- src/com/holub/application/presentation/payment/PaymentMethod.java
+++ src/com/holub/application/presentation/payment/PaymentMethod.java
@@ -1,2 +1,15 @@
-package com.holub.application.presentation.payment;public enum PaymentMethod {
+package com.holub.application.presentation.payment;
+
+public enum PaymentMethod {
+    CASH("cash"),
+    CREDIT_CARD("credit_card");
+    private final String method;
+
+    PaymentMethod(String method) {
+        this.method = method;
+    }
+
+    public String getMethod() {
+        return method;
+    }
 }

dec6eedfeb590226ac916e5820f813f2b3d2c968
Ukj0ng <xb253q@gmail.com>
2023-11-26 17:52:57 +0900

    feat(ConsoleMessage): 콘솔 메세지 카테고리 추가
    추가한 메세지:
    - 결제할 방법을 선택해주세요.
    - 원입니다. 결제해주세요.
    - 신용카드를 투입구에 끝까지 넣어주세요.
    - 결제 처리중입니다.
    - 주문이 성공적으로 진행되었습니다.

--- src/com/holub/application/presentation/message/ConsoleMessage.java
+++ src/com/holub/application/presentation/message/ConsoleMessage.java
@@ -4,6 +4,7 @@ import com.holub.application.constant.BeverageType;
 import com.holub.application.constant.BreadType;
 import com.holub.application.constant.SauceType;
 import com.holub.application.constant.ToppingType;
+import com.holub.application.presentation.payment.PaymentMethod;
 
 public enum ConsoleMessage {
     GREETING("xx 햄버거입니다."),
@@ -17,7 +18,13 @@ public enum ConsoleMessage {
     TOPPINGS_SELECTION("토핑의 종류를 선택해주세요."),
     BEVERAGE_TYPE(BeverageType.COFFEE.getName() + ", " + BeverageType.COKE.getName() + ", " + BeverageType.TEA.getName()),
     BEVERAGE_SELECTION("음료의 종류를 선택해주세요."),
-    ASK_FOR_CHANGE("선택한 메뉴에 수정하고 싶은 카테고리가 있나요? (빵/패티/소스/토핑/음료/없음)");
+    ASK_FOR_CHANGE("선택한 메뉴에 수정하고 싶은 카테고리가 있나요? (빵/패티/소스/토핑/음료/없음)"),
+    PAYMENT_TYPE(PaymentMethod.CASH.getMethod() + ", " + PaymentMethod.CREDIT_CARD.getMethod()),
+    PAYMENT_SELECTION("결제할 방법을 선택해주세요."),
+    CASH_PAYMENT("원입니다. 결제해주세요."),
+    CREDIT_CARD_PAYMENT("신용카드를 투입구에 끝까지 넣어주세요"),
+    CREDIT_CARD_PROCESSING("결제 처리중입니다."),
+    RESULT_MESSAGE("주문이 성공적으로 진행되었습니다.");
 
     private final String message;
 

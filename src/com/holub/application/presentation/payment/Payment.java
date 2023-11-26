2dd20265293bbad0e15061b43cb9ad3bb1731729
Ukj0ng <xb253q@gmail.com>
2023-11-26 17:49:17 +0900

    feat(Payment): 결제 객체 구현
    멤버 변수:
    - 결제 방법
    - 총 가격
    - 결제 날짜

--- src/com/holub/application/presentation/payment/Payment.java
+++ src/com/holub/application/presentation/payment/Payment.java
@@ -1,6 +1,15 @@
-package com.holub.application.presentation;
+package com.holub.application.presentation.payment;
+
+import java.time.LocalDate;
 
 public class Payment {
-    
+    private PaymentMethod method;
+    private double totalPrice;
+    private LocalDate paymentDate;
 
+    public Payment(PaymentMethod method, double totalPrice, LocalDate paymentDate) {
+        this.method = method;
+        this.totalPrice = totalPrice;
+        this.paymentDate = paymentDate;
+    }
 }

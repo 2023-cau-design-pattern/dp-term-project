6adfa7eae117d3301ac289eb5cd62ef86deaedc8
Ukj0ng <xb253q@gmail.com>
2023-11-28 13:09:17 +0900

    fix(Payment): 멤버 변수 변경
    변경 사항:
    - 결제 방법 -> 샌드위치

--- src/com/holub/application/presentation/payment/Payment.java
+++ src/com/holub/application/presentation/payment/Payment.java
@@ -1,15 +1,25 @@
 package com.holub.application.presentation.payment;
 
+import com.holub.application.sandwich.Sandwich;
 import java.time.LocalDate;
 
 public class Payment {
-    private PaymentMethod method;
+    private Sandwich sandwich;
     private double totalPrice;
     private LocalDate paymentDate;
 
-    public Payment(PaymentMethod method, double totalPrice, LocalDate paymentDate) {
-        this.method = method;
+    public Payment(Sandwich sandwich, double totalPrice, LocalDate paymentDate) {
+        this.sandwich = sandwich;
         this.totalPrice = totalPrice;
         this.paymentDate = paymentDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+            "sandwich=" + sandwich.getDescription() +
+            ", totalPrice=" + totalPrice +
+            ", paymentDate=" + paymentDate +
+            '}';
     }
 }
